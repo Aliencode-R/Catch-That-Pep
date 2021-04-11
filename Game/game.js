5def879f1a9111a001ef8e13788c45f84d0e5b48
Rishabh Sharma <risharma654@gmail.com>
2021-04-11 09:29:49 +0530

    changed restart windows locaion

--- Game/game.js
+++ Game/game.js
@@ -11,7 +11,7 @@ function main(currentTime) {
   if (gameOver) {
 
     if (confirm('You lost. Press ok to restart.')) {
-      window.location = '/'
+      window.location = 'index.html'
     }
 
     return
